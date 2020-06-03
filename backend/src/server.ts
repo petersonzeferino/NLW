925f04cf1f0cf5584178ccac30edaf02659cb2b5
Peterson Zeferino <petersonzeferino@hotmail.com>
2020-06-02 23:17:31 -0300

    Create Routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request,response) => {
-    console.log('Listagem de usuarios');
-
-    response.send('Helo World');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
