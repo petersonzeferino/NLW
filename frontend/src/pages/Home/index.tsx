0f083b1a4234fc14f38e0fdd81d1972bb708529e
Peterson Zeferino <petersonzeferino@hotmail.com>
2020-06-04 16:58:32 -0300

    Construct form HTML

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import './styles.css';
 import { FiLogIn }  from 'react-icons/fi'
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
@@ -16,12 +17,12 @@ const Home = () => {
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontratem seus pontos de coleta de forma eficiente.</p>
 
-                    <a href="/cadastro">
+                    <Link to="/create-point">
                         <span>
                             <FiLogIn />
                         </span>
                         <strong> Cadastre um ponto de coleta</strong>
-                    </a>
+                    </Link>
                 </main>
 
             </div>
