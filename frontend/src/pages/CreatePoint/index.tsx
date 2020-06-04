916a09173f05ff83e1f8ae0df424abc7596de2cb
Peterson Zeferino <petersonzeferino@hotmail.com>
2020-06-04 17:18:39 -0300

    Include Graphic

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft }  from 'react-icons/fi'
-
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 const CreatePoint = () => {
     return (
@@ -62,6 +62,16 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[51.453579, -0.09407]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                         />
+
+                         <Marker position={[51.453579, -0.09407]} />
+
+                    </Map>
 
 
                     <div className="field-group">
